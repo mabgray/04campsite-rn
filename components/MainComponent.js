f67ecca89abedf0bf0f25f9276918ddb54a95000
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-04-11 14:39:01 -0500

    redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -11,6 +11,15 @@ import SafeAreaView from 'react-native-safe-area-view';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+	fetchCampsites,
+	fetchComments,
+	fetchPromotions,
+	fetchPartners
+};
 
 const DirectoryNavigator = createStackNavigator(
 	{
@@ -176,6 +185,12 @@ const MainNavigator = createDrawerNavigator(
 const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
+	componentDidMount() {
+		this.props.fetchCampsites();
+		this.props.fetchComments();
+		this.props.fetchPromotions();
+		this.props.fetchPartners();
+	}
 	render() {
 		return (
 			<View
@@ -219,4 +234,4 @@ const styles = StyleSheet.create({
 	}
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
